bee4e6de17dd1794374373cde646bd6ede0fd7d4
NMINHDAI <ngominhdai92tn@gmail.com>
2022-05-19 23:04:48 +0700

    update full basic flow and model

--- controllers/shop.go
+++ controllers/shop.go
@@ -15,35 +15,40 @@ func AddShop(c *fiber.Ctx) error {
 		return err
 	}
 
-	var shopData models.Shop
-	err := json.Unmarshal([]byte(data["id"]), &shopData)
+	//var shopData models.Shop
 
-	if err != nil {
-		return err
+	var listProductId []uint
+	err1 := json.Unmarshal([]byte(data["id"]), &listProductId)
+	if err1 != nil {
+		return err1
 	}
+
+	var listQuantity []int32
+	err2 := json.Unmarshal([]byte(data["quantity"]), &listQuantity)
+	if err2 != nil {
+		return err2
+	}
+
+	var ownby uint
+	err3 := json.Unmarshal([]byte(data["ownby"]), &ownby)
+	if err3 != nil {
+		return err3
+	}
+
 	// {
-	// 	"id": [1,2,3],
+	// 	"id": "[1,2,3]",
 	// 	"quantity":[2,2,3]
 	// 	"ownby":"123"
 	// }
+	for i := 0; i < len(listProductId); i++ {
+		shopData := models.Shop{
+			ProductId: listProductId[i],
+			Quantity:  listQuantity[i],
+			OwnBy:     ownby,
+		}
 
-	// shopData := models.Shop{
-	// 	ProductId: data["id"],
-	// 	Quantity:  data["quantity"],
-	// 	OwnBy:     data["ownby"],
-	// }
-
-	database.DB.Create(&data)
-
-	// for i := 0; i < len(data["id"]); i++ {
-	// 	user := models.Shop{
-	// 		ProductId: data["id"][i],
-	// 		Quantity: data["quantity"][i],
-	// 		OwnBy: data["ownby"],
-	// 	}
-
-	// 	database.DB.Create(&user)
-	// }
+		database.DB.Create(&shopData)
+	}
 
 	return c.JSON(fiber.Map{
 		"message": "Successfully",
